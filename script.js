a5a8820605ab7f0556ec1916725b5565410fac7c
Akizenoko <carlmarcelomapa@gmail.com>
2026-03-08 20:12:26 +0800

    Chnaged theme to blue, added lots of changes

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const Btn = document.getElementById("profile");
+const sidebar = document.getElementById("sidebar");
+
+Btn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+document.addEventListener("click", (event) => {
+  if (!sidebar.contains(event.target) && !Btn.contains(event.target)){
+    sidebar.classList.remove("active");
+  }
+});
